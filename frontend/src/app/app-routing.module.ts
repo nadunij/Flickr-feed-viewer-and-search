37b69713c124836fbb6754989499c97018320357
Naduni Jayaweera <nadunij@gmail.com>
2021-02-17 17:11:37 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './ui/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      // Here we will add our application pages
+      { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+    ],
+  },
+  // { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
