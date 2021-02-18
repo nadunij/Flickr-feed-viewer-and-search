4af56af9898b052c82b90122aba29c4502da6abf
Naduni Jayaweera <nadunij@gmail.com>
2021-02-18 21:06:36 +0530

    Update flickrController.js
    Modify searchResults method

--- backend/src/controllers/flickrController.js
+++ backend/src/controllers/flickrController.js
@@ -1,4 +1,3 @@
-// import superagent from "superagent";
 import axios from "axios";
 import { GenerateErrorCode, GenerateResponse } from "../services/responseService.js";
 import config from "../config.js"
@@ -17,11 +16,10 @@ export const getAllPhotos = async (req, response) => {
         // console.log(photos.data.photos.photo);
    
         // "https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"
-        let returnres = {}
+        const returnres = {}
         photos.data.photos.photo.forEach(photo => {
         let image = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-        // returnres[photo.id] = image;
-        returnres[photo.id] = image;
+        returnres[photo.id] = image
         });
         response.status(200).json(GenerateResponse("Get all photos success",returnres));
   
@@ -36,12 +34,15 @@ export const searchResults = async (req, response) => {
     console.log("searchPhotos function start :p");
 
     try{
-        const searchPhotos =  await  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config.API_KEY}&tags=cat&format=json&nojsoncallback=true`) 
+        let tag = req.query;
+        console.log("tag.........   :p");
+        console.log(tag);
+        const searchPhotos =  await  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config.API_KEY}&tags=${tag.tag}&format=json&nojsoncallback=true`) 
         // console.log(typeof photos.data.photos.photo);    
-        console.log(searchPhotos.data.photos.photo);
+        // console.log(searchPhotos.data.photos.photo);
    
         // "https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"
-        let returnResponse = {}
+        const returnResponse = {}
         searchPhotos.data.photos.photo.forEach(photo => {
         let image = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
         returnResponse[photo.id] = image
