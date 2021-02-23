25b28336de15d9f7d932f2e11edd304f06c33133
Naduni Jayaweera <nadunij@gmail.com>
2021-02-23 18:56:30 +0530

    Create container component

---
+++ frontend/src/app/ui/container/container.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss']
+})
+export class ContainerComponent implements OnInit {
+
+  @Input()
+  public photos: Array<any> = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
