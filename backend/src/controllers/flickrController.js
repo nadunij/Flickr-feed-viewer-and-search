7dded2d4503a4d515677804ad3021da254719f5a
Naduni Jayaweera <nadunij@gmail.com>
2021-02-25 20:32:05 +0530

    Update flickrController.js

--- backend/src/controllers/flickrController.js
+++ backend/src/controllers/flickrController.js
@@ -7,27 +7,29 @@ export const getAllPhotos = async (req, response) => {
     console.log("getAllPhotos function start :p");
 
     try{
-        // const photos =  await  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${config.API_KEY}&format=json&nojsoncallback=true`) 
         const photos = await axios.get("https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true")  
-        // console.log(photos.data.photos.photo);
         // console.log(photos.data.items);
-   
-        // "https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"
+
         let returnres = [];
-        // photos.data.photos.photo.forEach(photo => {
-        // let image = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-        // // returnres[photo.title] = image;
-        // returnres.push({'title':photo.title, 'url':image});
-        // });
+
         photos.data.items.forEach(photo => {
-            //key-value pair
-            // returnres[photo.title] = photo.media.m;
-            //object
             returnres.push({'title':photo.title, 'url':photo.media.m});
         });
 
         response.status(200).json(GenerateResponse("Get all photos success",returnres));
-  
+
+        // const photos =  await  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${config.API_KEY}&format=json&nojsoncallback=true`)
+        // // console.log(photos.data.photos.photo);
+
+        // let returnres = [];
+
+        // photos.data.photos.photo.forEach(photo => {
+        // let image = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+        // returnres.push({'title':photo.title, 'url':image});
+        // });
+
+        // response.status(200).json(GenerateResponse("Get all photos success",returnres));
+
     } catch(err){
         console.log(err);
     }
@@ -40,34 +42,41 @@ export const searchResults = async (req, response) => {
 
     try{
         let tags = [req.query.tag];
-        console.log("tag.........   :p");
         console.log(tags);
+
         //remove empty tags
         let processedTags = tags.filter(function (el) {
             return el != null;
           });
+
         //Encodes element so it can be passed in URL, ex whitespaces become %20
         processedTags = processedTags.map((el) => encodeURI(el));
 
         let tagList = processedTags.join(',');
-        // const searchPhotos =  await  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config.API_KEY}&tags=${tag.tag}&format=json&nojsoncallback=true`) 
-        // const searchPhotos =  await  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config.API_KEY}&tags=${tagList}&format=json&nojsoncallback=true`)
+
         const searchPhotos =  await  axios.get(`https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true&tags=${tagList}`)   
-        // console.log(searchPhotos.data.photos.photo);
         // console.log(searchPhotos.data.items);
    
-        // "https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"
         const returnResponse = [];
-        // searchPhotos.data.photos.photo.forEach(photo => {
-        // let image = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-        // // returnResponse[photo.title] = image;
-        // returnResponse.push({'title':photo.title, 'url':image});
-        // });
+
         searchPhotos.data.items.forEach(photo => {
             returnResponse.push({'title':photo.title, 'url':photo.media.m});
         });
         response.status(200).json(GenerateResponse("Get all photos success", returnResponse));
   
+        // let tag = req.query;
+        // const searchPhotos =  await  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config.API_KEY}&tags=${tag.tag}&format=json&nojsoncallback=true`) 
+
+        // const searchPhotos =  await  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config.API_KEY}&tags=${tagList}&format=json&nojsoncallback=true`)
+        // console.log(searchPhotos.data.photos.photo);
+
+        // const returnResponse = [];
+        // searchPhotos.data.photos.photo.forEach(photo => {
+        // let image = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+        // returnResponse.push({'title':photo.title, 'url':image});
+        // });
+        // response.status(200).json(GenerateResponse("Get all photos success", returnResponse));
+
     } catch(err){
         console.log(err);
     }
